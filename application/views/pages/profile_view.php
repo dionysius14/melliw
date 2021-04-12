f49729ae6f3d4b4b1fd4e231d64398587d590515
ramaap <rama.adyasa@gmail.com>
2021-04-12 22:34:02 +0700

    edit product list

--- application/views/pages/profile_view.php
+++ application/views/pages/profile_view.php
@@ -5,65 +5,61 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 <div class="container">
 	<div class="profile container">
 		<img src="<?php echo base_url(); ?>assets/uploads/banner_profile.jpg">
-		<div class="content">
-			<?php echo $profile->profile_ket; ?>
-		</div>
 		<div class="story">
 		  	<h1>Story Behind</h1>
-	  		<p>
-	  			Lorem Ipsum has been the industry’s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. Lorem Ipsum has been the industry’s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.
-	  			Lorem Ipsum has been the industry’s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.
-	  			Lorem Ipsum has been the industry’s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.
-	  			Lorem Ipsum has been the industry’s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.
-	  		</p>
+			<div class="content">
+				<?php echo $profile->profile_ket; ?>
+			</div>
 		</div>
-		<div class="side">
-		  	<h1>Vision</h1>
-		  	<div class="points">
-		  		<i class="icon-thumbs-up-alt" ></i>
-		  		<h2>High quality service</h2>
-		  		<p>
-		  			Lorem Ipsum has been the industry’s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.
-		  		</p>
-		  	</div>
-		  	<div class="points">
-		  		<i class="fa fa-users"></i>
-		  		<h2>Satisfied customers</h2>
-		  		<p>
-		  			Lorem Ipsum has been the industry’s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.
-		  		</p>
-		  	</div>
-		  	<div class="points">
-		  		<i class="fa fa-certificate"></i>
-		  		<h2>High quality products</h2>
-		  		<p>
-		  			Lorem Ipsum has been the industry’s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.
-		  		</p>
-		  	</div>
-		</div>
-		<div class="side">
-		  	<h1>Mission</h1>
-		  	<div class="points">
-		  		<i class="icon-thumbs-up-alt" ></i>
-		  		<h2>Untuk Lingkungan</h2>
-		  		<p>
-		  			Lorem Ipsum has been the industry’s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.
-		  		</p>
-		  	</div>
-		  	<div class="points">
-		  		<i class="fa fa-users"></i>
-		  		<h2>Untuk Konsumen</h2>
-		  		<p>
-		  			Lorem Ipsum has been the industry’s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.
-		  		</p>
-		  	</div>
-		  	<div class="points">
-		  		<i class="fa fa-certificate"></i>
-		  		<h2>Untuk Shareholder</h2>
-		  		<p>
-		  			Lorem Ipsum has been the industry’s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.
-		  		</p>
-		  	</div>
+		<div>
+			<div class="side">
+			  	<h1>Vision</h1>
+			  	<div class="points">
+			  		<i class="icon-thumbs-up-alt" ></i>
+			  		<h2>High quality service</h2>
+			  		<p>
+			  			Lorem Ipsum has been the industry’s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.
+			  		</p>
+			  	</div>
+			  	<div class="points">
+			  		<i class="fa fa-users"></i>
+			  		<h2>Satisfied customers</h2>
+			  		<p>
+			  			Lorem Ipsum has been the industry’s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.
+			  		</p>
+			  	</div>
+			  	<div class="points">
+			  		<i class="fa fa-certificate"></i>
+			  		<h2>High quality products</h2>
+			  		<p>
+			  			Lorem Ipsum has been the industry’s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.
+			  		</p>
+			  	</div>
+			</div>
+			<div class="side">
+			  	<h1>Mission</h1>
+			  	<div class="points">
+			  		<i class="icon-thumbs-up-alt" ></i>
+			  		<h2>Untuk Lingkungan</h2>
+			  		<p>
+			  			Lorem Ipsum has been the industry’s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.
+			  		</p>
+			  	</div>
+			  	<div class="points">
+			  		<i class="fa fa-users"></i>
+			  		<h2>Untuk Konsumen</h2>
+			  		<p>
+			  			Lorem Ipsum has been the industry’s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.
+			  		</p>
+			  	</div>
+			  	<div class="points">
+			  		<i class="fa fa-certificate"></i>
+			  		<h2>Untuk Shareholder</h2>
+			  		<p>
+			  			Lorem Ipsum has been the industry’s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.
+			  		</p>
+			  	</div>
+			</div>
 		</div>
 		<div class="stucture">
 		  	<h1>Organization Stucture</h1>
