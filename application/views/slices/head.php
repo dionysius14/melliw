af0debdc58c285b807220eb4aad3df28ba856c26
ramaap <rama.adyasa@gmail.com>
2021-06-05 23:10:46 +0700

    Rombak total : Homepage + header + footer (desktop)

--- application/views/slices/head.php
+++ application/views/slices/head.php
@@ -43,7 +43,6 @@ echo add_js($folder . 'jquery.fancybox.min');
         <link href="https://fonts.googleapis.com/css?family=Lobster" rel="stylesheet">
         <link href="https://fonts.googleapis.com/css?family=Roboto" rel="stylesheet">
         <link href="https://fonts.googleapis.com/css?family=PT+Sans" rel="stylesheet">
-        <link rel="stylesheet" href="/assets/css/public/default/font-awesome.min.css">
         <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.4.1/css/all.css" integrity="sha384-5sAR7xN1Nv6T6+dT2mhtzEpVJvfS3NScPQTrOxhwjIuvcA67KV2R5Jz6kr4abQsz" crossorigin="anonymous">
 <script type="text/javascript">
     
